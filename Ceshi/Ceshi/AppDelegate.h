7d48578734b3e5065e2b6bbde4c926dd5f2a2935
ESLYanBo <784453650@qq.com>
2018-03-01 15:05:36 +0800

    首次上传
    增加正则式验证信息

---
+++ Ceshi/Ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ceshi
+//
+//  Created by 王晨辉 on 2018/1/9.
+//  Copyright © 2018年 王晨辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ceshi/Ceshi/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  Ceshi
+//
+//  Created by 王晨辉 on 2018/1/9.
+//  Copyright © 2018年 王晨辉. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "RegularVerification.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)sendBtn:(UIButton *)sender {
+
+    if ([RegularVerification checkUserName:@"我的是你的"]) {
+        NSLog(@"YES");
+    }else{
+        NSLog(@"NO");
+    }
+    
+
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
